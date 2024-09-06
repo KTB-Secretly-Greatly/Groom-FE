55c181bad21aba50e1696dfa50846098efc85d32
cjm0611 <cjm-0611@naver.com>
2024-09-07 03:54:42 +0900

    style: 텍스트 크기 조정

--- src/components/MyMessage.tsx
+++ src/components/MyMessage.tsx
@@ -14,16 +14,18 @@ const MyMessage = ({
   return (
     <div className="text-right">
       <div className="flex items-center flex-row-reverse">
-        <p className="ml-[5px]">{nickname}</p>
+        <p className="ml-[5px] text-[14px]">{nickname}</p>
         <img
-          className="w-[30px] h-[30px] rounded-full bg-cover bg-center mb-[5px]"
+          className="w-[20px] h-[20px] rounded-full bg-cover bg-center mb-[5px]"
           src={profileImage}
           alt={`${nickname}의 프로필 이미지`}
         />
       </div>
       <div className="flex justify-end items-end mb-[10px]">
         <p className="text-[12px] text-timestamp-text mr-[5px]">{timestamp}</p>
-        <p className="p-2 rounded-lg inline-block bg-self-msg-bg">{message}</p>
+        <p className="p-2 rounded-lg inline-block bg-self-msg-bg text-[14px]">
+          {message}
+        </p>
       </div>
     </div>
   );
