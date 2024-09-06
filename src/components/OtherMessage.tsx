0bfe93f68fed6f9f3fbe2760e5e4557702b6c4fb
cjm0611 <cjm-0611@naver.com>
2024-09-07 04:52:32 +0900

    style: 다른 사람의 메세지 UI 수정

--- src/components/OtherMessage.tsx
+++ src/components/OtherMessage.tsx
@@ -15,11 +15,11 @@ const OtherMessage = ({
     <div>
       <div className="flex items-center">
         <img
-          className="w-[30px] h-[30px] rounded-full bg-cover bg-center mb-[5px]"
+          className="w-[20px] h-[20px] rounded-full bg-cover bg-center mb-[5px]"
           src={profileImage}
           alt={`${nickname}의 프로필 이미지`}
         />
-        <p className="ml-[5px]">{nickname}</p>
+        <p className="ml-[5px] text-[14px]">{nickname}</p>
       </div>
       <p className="text-[14px] mb-[10px] relative">
         <span className="p-2 rounded-lg inline-block bg-white self-start">
