70665f40bc238b8f51ec7bc04123af2a81e5e22c
cjm0611 <cjm-0611@naver.com>
2024-09-06 17:03:49 +0900

    feat: 웹소켓 통신 연결 및 메세지 전송

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import MessageInput from './components/MessageInput';
 import { MessageType } from './types/MessageType';
@@ -49,6 +49,48 @@ function App() {
 
   const currentUserNickname = 'me';
 
+  const wsRef = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    if (currentUserNickname) {
+      wsRef.current = new WebSocket('ws://localhost:8080/chat');
+
+      wsRef.current.onopen = () => console.debug('Connection opened');
+
+      wsRef.current.onmessage = event => {
+        try {
+          const receivedMessage = JSON.parse(event.data); // JSON으로 파싱
+          const newChatLog: MessageType = {
+            nickname: receivedMessage.nickname,
+            content: receivedMessage.content,
+            timestamp: new Date().toLocaleTimeString('ko-KR', {
+              hour: 'numeric',
+              minute: 'numeric',
+              hour12: true, // 12시간 형식 (오전/오후)
+            }),
+          };
+          setChatsLogs(prevChatLogs => [...prevChatLogs, newChatLog]);
+        } catch (error) {
+          console.error('Failed to parse WebSocket message', error);
+        }
+      };
+
+      wsRef.current.onclose = event => {
+        if (event.wasClean) {
+          console.debug(
+            `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`,
+          );
+        } else {
+          console.debug('[close] Connection died');
+        }
+      };
+
+      return () => wsRef.current?.close();
+    }
+
+    return undefined;
+  }, [currentUserNickname]);
+
   const addMessageToChatLogs = (newMessage: string, nickname: string) => {
     const currentTime = new Date();
     const newChatLog = {
@@ -60,6 +102,19 @@ function App() {
         hour12: true, // 12시간 형식 (오전/오후)
       }),
     };
+
+    // WebSocket을 통해 서버로 메시지 전송
+    if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+      const newData = {
+        messageType: 'TALK', // ENTER, TALK
+        chatRoomId: 1, // 채팅방 번호
+        senderId: 1, // 메세지 전송자의 UserId
+        message: newMessage, // 메세지 내용
+      };
+      wsRef.current.send(JSON.stringify(newData));
+    }
+
+    // 로컬 상태에 메시지 추가
     setChatsLogs([...chatLogs, newChatLog]);
   };
 
