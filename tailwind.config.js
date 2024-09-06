3ab56f8e490ef927f7426e2b1c53ac520fc1103e
cjm0611 <cjm-0611@naver.com>
2024-09-06 15:48:00 +0900

    style: 컬러 시트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
+export const theme = {
+  extend: {
+    colors: {
+      'chat-bg': '#9bbbd4',
+      'other-msg-bg': '#ffffff',
+      'self-msg-bg': '#fef01b',
+      'timestamp-text': '#556677',
+    },
   },
-  plugins: [],
-}
-
+};
+export const plugins = [];
